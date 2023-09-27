1cd03666cd74d4c6919c9115242d4bbd5c9e9bec
unknown <cjsteward07@gmail.com>
2023-09-27 13:53:49 -0400

    completed exercise 1.4

---
+++ Exercises/Exercise 1.4 - Adding Two Floats/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Write("Please enter a number: ");
+float firstInput = float.Parse(Console.ReadLine());
+
+Console.Write("Please enter a second number: ");
+float secondInput = float.Parse(Console.ReadLine());
+
+float inputTotal = firstInput + secondInput;
+
+Console.WriteLine($"\nThe total of your numbers '{firstInput}' and '{secondInput}' is {inputTotal}");
+
