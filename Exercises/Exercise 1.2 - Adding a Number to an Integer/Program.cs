5924440d26a997197d0f631974d3eb71f3ed6d16
unknown <cjsteward07@gmail.com>
2023-09-27 13:39:19 -0400

    completed 1.2 exercise

---
+++ Exercises/Exercise 1.2 - Adding a Number to an Integer/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Write("Hello, please enter a number: ");
+
+int userNumber = int.Parse(Console.ReadLine());
+
+int userNumberPlusOne = userNumber + 1;
+
+Console.WriteLine($"\n your new number is {userNumberPlusOne}");
