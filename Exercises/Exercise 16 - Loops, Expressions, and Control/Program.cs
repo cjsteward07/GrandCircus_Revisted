fb0292749c3b2ae6af6e4bd687e10be5fb14fe5b
unknown <cjsteward07@gmail.com>
2023-09-27 18:07:36 -0400

    completed exercise 16

---
+++ Exercises/Exercise 16 - Loops, Expressions, and Control/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+string userContinue;
+int triangleHeight = 10;
+do
+{
+    Console.Clear();
+
+    for (int i = 0; i < triangleHeight; i++)
+    {
+        for (int n = 0; n <= i; n++)
+        {
+            Console.Write("*");
+        }
+
+        Console.Write("\n");
+    }
+
+    Console.WriteLine("\nWould you like to continue(y/n)?: ");
+    userContinue = Console.ReadLine();
+
+} while (userContinue == "y");
