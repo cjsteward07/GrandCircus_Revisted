8f76346e34ad7d7c9c888875dab9c8877a7a7753
unknown <cjsteward07@gmail.com>
2023-09-27 18:24:48 -0400

    exercise 17 triangle wip

---
+++ Exercises/Exercise 17 - Loops, Expressions, and Control/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+const int triangleHeight = 10;
+string userContinue;
+
+do
+{
+    for (int i = 0; i < triangleHeight; i++)
+    {
+
+        for (int n = 0; n <= 1 * i; n++)
+            Console.Write("*");
+        Console.Write("\n");
+    }
+
+
+
+
+    Console.Write("Would you like to continue (y/n)?: ");
+    userContinue = Console.ReadLine();
+
+} while (userContinue == "y");
