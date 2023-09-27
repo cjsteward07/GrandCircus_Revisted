e3a39b4e3d595b8d1b26a8abb768d309bae5f01b
unknown <cjsteward07@gmail.com>
2023-09-27 14:07:10 -0400

    completed exercise 1.5

---
+++ Exercises/Exercise 1.5 - Multiplying Floats/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Write("Please enter a number: ");
+float firstUserInput = float.Parse(Console.ReadLine());
+
+Console.Write("Please enter a second number: ");
+float secondUserInput = float.Parse(Console.ReadLine());
+
+float userInputMultiplied = firstUserInput * secondUserInput;
+
+Console.WriteLine($"\n The multiplied total of your numbers '{firstUserInput}' and '{secondUserInput}' is {userInputMultiplied}");
