5ff4497d990145da930f38e334388b3800cbf793
unknown <cjsteward07@gmail.com>
2023-09-27 13:02:37 -0400

    completed first lab

---
+++ Exercises/Exercise 1.1 - Echo String/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Echo String Exercise");
+
+Console.Write("Please enter a word: ");
+string userText = Console.ReadLine();
+
+Console.Write("\nYou have entered: ");
+Console.Write(userText);
