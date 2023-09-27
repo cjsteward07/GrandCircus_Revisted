b67cd88c4e8519b4c02c746a7354addff54c51dc
unknown <cjsteward07@gmail.com>
2023-09-27 17:03:29 -0400

    exercise 12 complete

---
+++ Exercises/Exercise 12 - Loops, Expressions, and Control/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+string userContinue = "y";
+
+do
+{
+    for (int i = 9; i > -1; i--)
+    {
+        Console.Write($"{i} ");
+    }
+
+    Console.WriteLine("\nWould you like to run again (y/n)?");
+    userContinue = Console.ReadLine();
+
+} while (userContinue == "y");
+
+Console.WriteLine("Goodbye!");
