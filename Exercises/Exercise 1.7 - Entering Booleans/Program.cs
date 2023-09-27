cffd9d58869e627cfbe05cba9a4e9bb372dd151d
unknown <cjsteward07@gmail.com>
2023-09-27 14:43:35 -0400

    completed exercise 1.7

---
+++ Exercises/Exercise 1.7 - Entering Booleans/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+bool boolToReturn = false;
+
+Console.Write("Please enter a boolean (example: 'True' or 'False'): ");
+
+bool userInput = bool.Parse(Console.ReadLine());
+
+if (userInput == false)
+{
+    boolToReturn = true;
+}
+else if (userInput == true)
+{
+    boolToReturn = false;
+}
+
+Console.WriteLine($"\nThe return boolean would be {boolToReturn} since you entered {userInput}");
