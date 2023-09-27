643b0d903b1521b1eb4ccc3cc9d23ef300f55e62
unknown <cjsteward07@gmail.com>
2023-09-27 15:59:51 -0400

    completed exercise 6 do while loop

---
+++ Exercises/Exercise 6 - Loops, Expressions, and Control/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//do while loops will always run once because it doesn't read the while condition until after the do block is processed.
+//variables used as the while condition need to be declared outside of the loops otherwhise the variable will get re-initialized. Remember scope
+
+string userInput;
+
+do
+{
+    Console.WriteLine("Hello, World!");
+    Console.Write("Would you like to continue (y/n)?: ");
+    userInput = Console.ReadLine();
+
+} while (userInput == "Yes" || userInput == "yes" || userInput == "Y" || userInput == "y");
