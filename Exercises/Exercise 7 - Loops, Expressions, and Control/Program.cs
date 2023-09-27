703736eb4b4870fc4fd8f1676242655900395645
unknown <cjsteward07@gmail.com>
2023-09-27 16:09:38 -0400

    completed exercise 7

---
+++ Exercises/Exercise 7 - Loops, Expressions, and Control/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string userLoop;
+do
+{
+    Console.Clear();
+
+    Console.WriteLine("Echo String Exercise");
+
+    Console.Write("Please enter a word: ");
+    string userText = Console.ReadLine();
+
+    Console.Write("\nYou have entered: ");
+    Console.WriteLine(userText);
+
+    Console.Write("Would you like to continue (y/n)?: ");
+    userLoop = Console.ReadLine();
+
+} while (userLoop == "Yes" || userLoop == "yes" || userLoop == "Y" || userLoop == "y");
+
+Console.WriteLine("Goodbye!");
