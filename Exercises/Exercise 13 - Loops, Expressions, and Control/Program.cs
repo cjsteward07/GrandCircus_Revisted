c641b33cbb7fc826d270870346e406903d1c74d0
unknown <cjsteward07@gmail.com>
2023-09-27 17:12:19 -0400

    exercise 13 complete

---
+++ Exercises/Exercise 13 - Loops, Expressions, and Control/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string userContinue;
+do
+{
+    Console.Clear();
+    Console.Write("Please enter an integer: ");
+
+    int userInteger = int.Parse(Console.ReadLine());
+
+    for (int i = -1; i < userInteger; userInteger--)
+    {
+        Console.Write($"{userInteger} ");
+    }
+
+    Console.WriteLine("\nWould you like to run again (y/n)?");
+    userContinue = Console.ReadLine();
+
+} while (userContinue == "y");
+
+Console.WriteLine("Goodbye!");
