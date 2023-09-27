1902dadbfd40506f2b8e210acfaac9e1ab544fb9
unknown <cjsteward07@gmail.com>
2023-09-27 13:49:40 -0400

    completed exercise 1.3

---
+++ Exercises/Exercise 1.3 - Adding a Number to a Float/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Write("Please enter a number: ");
+
+float userInput = float.Parse(Console.ReadLine());
+
+float newFloat = userInput + .5f;
+
+Console.WriteLine($"\nYour new floating number is {newFloat}");
